cc3e76fc1db61b81f30d2b52b84386c1fe6d0bef
Amdatalab <nikitaamdatalab@gmail.com>
2023-10-31 10:13:07 -0700

    add

---
+++ js/App.js
@@ -0,0 +1,253 @@
+var districtTalukaVillageinfo = {
+    Palghar: {
+        Talasari: {
+            Zai: "Zai"
+        },
+        Dahanu: {
+            Bordi: "Bordi", Gholwad: "Umali", Vikasnager: "Vikasnager", Chikhale: "Chikhale",
+            Ambewadi: "Ambewadi", Narpad: "Narpad", Thakurwadi: "Thakurwadi", Dahanu: "Dahanu",
+            'Dhakti Dahanu': "Dhakti Dahanu", 'Bade Pokhran': "Bade Pokhran", Dhumket: "Dhumket",
+            Chandigaon: "Chandigaon", Gungawada: "Gungawada", Pokhran: "Pokhran", vandhavan: "vandhavan",
+            Vasgaon: "Vasgaon", Ambistewadi: "Ambistewadi", Varor: "Varor", Chinchani: "Chinchani"
+        },
+        Palghar: {
+            'Tarapur(CT)': "Tarapur(CT)", Kambode: "Kambode", Sawarai: "Sawarai", Ghivali: "Ghivali",
+            Akkarptti: "Akkarptti", Popharan: "Popharan", Unbhat: "Unbhat", Uchcheli: "Uchcheli",
+            dandi: "dandi", Nawapur: "Nawapur", Tembhi: "Tembhi",
+            Alewadi: "Alewadi", Gundavali: "Gundavali", Murabe: "Murabe", Satpati: "Satpati",
+            'Shirgaon(CT)': "Shirgaon(CT)", Wadrai: "Wadrai", Haranawadi: "Haranawadi", Nandgaon: "Nandgaon",
+            Mahim: "Mahim", Kelwe: "Kelwe", Mangelwada: "Mangelwada", Usarni: "Usarni",
+            Dande: "Dande", Khatali: "Khatali", Mathane: "Mathane", Edwan: "Edwan",
+            Kore: "Kore", Datiware: "Datiware", Bandar: "Bandar"
+        },
+        Vasai: {
+            Chikhaldongre: "Chikhaldongre", Kolhapur: "Kolhapur", Mukkam: "Mukkam", Patilgaon: "Patilgaon",
+            Arnala: "Arnala", Vatar: "Vatar", Satpale: "Satpale", Rajodi: "Rajodi",
+            Kalamb: "Kalamb", Wagholi: "Wagholi", Nirmal: "Nirmal",
+            'Bhugaon KH': "Bhugaon KH", 'Bhugaon BK': "Bhugaon BK", Rangaon: "Rangaon", 'Kaular BK': "Kaular BK",
+            'Kaular KH': "Kaular KH", 'Sandur(CT)': "Sandur(CT)", 'Vasai(M CI)': "Vasai(M CI)", Kochiwade: "Kochiwade",
+            Vadiwali: "Vadiwali", Panju: "Panju"
+        }
+    },
+    Thane: {
+        Thane: {
+            Bhaindar: "Bhaindar", Chouk: "Chouk", Dongari: "Dongari", Pali: "Pali",
+            Morva: "Morva", Raimurdhe: "Raimurdhe", Taraandi: "Taraandi", Uttan: "Uttan"
+        }
+    },
+    'Mumbai Suburban': {
+        'Mumbai Suburban': {
+        },
+        Andheri: {
+            Bandra: "Bandra", Danda: "Danda", Juhu: "Juhu", Madhali: "Madhali",
+        },
+        Kurla: {
+            Anik: "Anik", Chembur: "Chembur", Bhandup: "Bhandup", Mulund: "Mulund",
+            Hariyali: "Hariyali", Kanjur: "Kanjur", Mahul: "Mahul", Paspoli: "Paspoli",
+            Pawai: "Pawai", Turbhe: "Turbhe", Vadhavali: "Vadhavali", Vikroli: "Vikroli"
+        },
+        Borivli: {                                                                                                  
+            Akshe: "Akshe", Charcop: "Charcop", Chinchwali: "Chinchwali", Dharvali: "Dharvali",
+            Eksar: "Eksar", ' Eksar Pahadi': "Eksar Pahadi", Erangal: "Erangal", Gorai: "Gorai",
+            Kalerabad: "Kalerabad", Kandivali: "Kandivali", Malad: "Malad", Malwani: "Malwani",
+            Manori: "Manori", Marve: "Marve", Wadhavan: "Wadhavan", Valnai: "Valnai"
+        }
+    },
+    Raigad: {
+        Panvel: {
+            Targhar: "Targhar",Ulawe: "Ulawe",Wahal: "Wahal",Kharkopar: "Kharkopar",Gavhan: "Gavhan",Nhave: "Nhave"
+        },
+        Uran: {
+            Gharapuri: "Gharapuri", 'Juna Sheva': "Juna Sheva", Jaskhar: "Jaskhar", Panje: "Panje",
+            Funde: "Funde", Dongari: "Dongari", 'Hanuman Koliwada': "Hanuman Koliwada", Boripakhadi: "Boripakhadi",
+            Ranwad: "Ranwad", 'Kegaon(CT) ': "Kegaon(CT) ", Nagaon: "Nagaon",
+            Mhatwali: "Mhatwali", 'Chanje (CT)': "Chanje (CT)", 'Navin Sheva': "Navin Sheva", Bhendkhal: "Bhendkhal",
+            'Navghar(CT)': "Navghar(CT)", Dhasakhosi: "Dhasakhosi", Bandhpada: "Bandhpada", Kacherpada: "Kacherpada",
+            Pirkone: "Pirkone", Sangpalekhar: "Sangpalekhar", Govthane: "Govthane", Aware: "Aware",
+        },
+        Pen: {
+            VitthalWadi: "VitthalWadi",'Mothe Bhal': "Mothe Bhal",Lakhola: "Lakhola"
+        },
+        Alibag: {
+            Ramkotha: "Ramkotha", Sonakothakhar: "Sonakothakhar", Hashiware: "Hashiware", Kawade: "Kawade",
+            'Ranjankhar Davali': "Ranjankhar Davali", Milkatkhar: "Milkatkhar", Belpada: "Belpada", Rewas: "Rewas",
+            Pupadevipada: "Pupadevipada", Saral: "Saral", Virtasaral: "Virtasaral",
+            Yelwane: "Yelwane", 'Mandava Tarf Zirad': "Mandava Tarf Zirad", Dhokawade: "Dhokawade", Murabe: "Murabe",
+            Kolgaon: "Kolgaon", Navedar: "Navedar", 'Navedar Kolgaon': "Navedar Kolgaon", Sasawane: "Sasawane",
+            Awas: "Awas", Surekhar: "Surekhar", Agarsure: "Agarsure", Bamansure: "Bamansure",
+            Kihim: "Kihim", 'Nevedar beli': "Nevedar beli", 'Navagaon Boris': "Navagaon Boris", Thal: "Thal",
+            Lonare: "Lonare", Varasoli: "Varasoli", 'Chendhare(CT)': "Chendhare(CT)",
+             'Alibag(MCI)': "Alibag(MCI)", 'Kurul(CT)': "Kurul(CT)", Akshi: "Akshi", Navedar: "Navedar",
+            Nagaon: "Nagaon",Choul: "Choul", Revdanda: "Revdanda", Agarkot: "Agarkot",
+        },
+        Murud: {
+            Salav: "Salav", Korlai: "Korlai", Borli: "Borli", Surai: "Surai",
+            Kolmandale: "Kolmandale", Mandala: "Mandala", Barashiv: "Barashiv", Tembhode: "Tembhode",
+            Bhoighar: "Bhoighar", Kashid: "Kashid", Sarve: "Sarve",
+            'Donde tarf nandgaon': "Donde tarf nandgaon", 'Adhi (pangole)': "Adhi (pangole)", Nandgaon: "Nandgaon", 
+            'Kharik Wada': "Kharik Wada",
+            Khardodkule: " Khardodkule", Mazgaon: "Mazgaon", 'Sarane nandgaon': "Sarane nandgaon", More: "More",
+            Vitour: "Vitour", 'Murud(Rural)': "Panju",
+            'Murud janjira(MCI)': "Murud janjira(MCI)", Eakdara: "Eakdara", Telwade: "Telwade", Rajpuri: "Rajpuri",
+            Amboli: "Amboli", Haphukhar: "Haphukhar", Agardande: "Agardande", Nandale: "Nandale",
+            Usadi: "Usadi", Tokekhar: "Tokekhar", Sawali: "Sawali",
+            Mithekhar: "Mithekhar", Khambde: "Khambde"
+        },
+        Tala: {
+            Kude: "Kude",Rowala: "Rowala", 'Washi Haveli ': "Washi Haveli "
+        },
+        Mhasala: {
+            Waral: "Waral",Kalsuri: "Kalsuri",Turumbade: "Turumbade",
+            Rohinikond: "Rohinikond",'Adi Thakur': "Adi Thakur"
+        },
+        Shrivardhan: {
+            Harvil: "Harvil", Khudgaon: "Khudgaon", Karlas: "Karlas", Dighi: "Dighi",
+            Nanavali: "Nanavali", Maneri: "Maneri", Surve: "Surve", Adgaon: "Adgaon",
+            Kunbiwadi: "Kunbiwadi", 'Velas Agar': "Velas Agar", Velas: "Velas",
+            Vadavali: "Vadavali", DiveAgar: "DiveAgar", Karle: "Karle", Bharadkhed: "Bharadkhed",
+            Shekhadi: "Shekhadi", Kondivali: "Kondivali", Khargaon: "Khargaon", Walwati: "Walwati",
+            'Walwati KH.': "Walwati KH.", 'Shrivardhan(MCI)': "Shrivardhan(MCI)",
+            Shrivardhan: "Shrivardhan", Kalinje: "Kalinje", Kuravade: "Kuravade", Maral: "Maral",
+            Devaghar: "Devaghar", Danda: "Danda", Bagmandale: "Bagmandale"
+        }
+    },
+    Ratnagiri: {
+        Mandangad: {
+            'Walmiki Nagar': "Walmiki Nagar",Bankot: "Bankot",'Narayam Nagar': "Narayam Nagar",Velas: "Velas",Sakhari: "Sakhari",Nhave: "Nhave"
+        },
+        Dapoli: {
+            Kelashi: "Kelashi", Utambar: "Utambar", Ade: "Ade", Padale: "Padale",
+            Anjarla: "Anjarla", Tadachakond: "Tadachakond", Murdi: "Murdi",
+             Pandhari: "Pandhari",
+            'Juikar Mohalla': "Juikar Mohalla", Harnal: "Harnal ", Adkhal: "Adkhal",
+            Saldure: "Saldure", Asud: "Asud", 'Ambivali KH ': "Ambivali KH", Murud: "Murud",
+            Karde: "Karde", Chandranagar: "Chandranagar", Ladghar: "Ladghar", 
+            Tamastirth: "Tamastirth",
+            Buradi: "Buradi", Teleshwarbagar: "Teleshwarbagar", "Shital Nagar": "Shital Nagar",
+             "Mahamay Nagar": "Mahamay Nagar", Kolthare: "Kolthare", Borivali: "Borivali", 
+             Panchanadi: "Panchanadi", Aghari: "Aghari",
+             "Vanoshi Tarf Panchanadi": "Vanoshi Tarf Panchanadi", Dabhol: "Dabhol"
+        },
+        Guhagar: {
+            Navanagar: "Navanagar",'Gharatwadi Tarf Veldur': "Gharatwadi Tarf Veldur",'Mouje Anjanvel': "Mouje Anjanvel",
+            'Peth Anjanvel': "Peth Anjanvel", 'Katalwadi Tarf Anjanwel': "Katalwadi Tarf Anjanwel", Ranavi: "Ranavi", 
+            'Varchapath Tarf Guhagar': "Varchapath Tarf Guhagar",
+            Are: "Are", Guhagar: "Guhagar", 'Kirtanwadi Tarf Guhaghar': "Kirtanwadi Tarf Guhaghar", Asgoli: "Asgoli",
+            Palshet: "Palshet", 'Maruti Mandirwadi': "Maruti Mandirwadi", Adoor: "Adoor",
+            'Kond Karul': "Kond Karul", Velaneshvar: "Velaneshvar", 'Sakhari Agar': "Sakhari Agar", Hedavi: "Hedavi",
+            Naravan: "Naravan", Karde: "Karde", Rohile: "Rohile",Tavasalwadi: "Tavasalwadi",
+            Padave: "Padave", Tavasal: "Tavasal"
+        },
+        Ratnagiri: {
+            'Sakhar Mohalla': "Sakhar Mohalla", Jaigad: "Jaigad", Nandivade: "Nandivade", Sandelavagan: "Sandelavagan",
+            Sandkhol: "Sandkhol", Kachare: "Kachare", Chaperi: "Chaperi", Undi: "Undi",
+            Reel: "Reel", Marathwada: "Marathwada", Bhandarwada: "Bhandarwada",
+            Kunbiwadi: "Kunbiwadi",Waravade: "Waravade", Malgund: "Malgund", Bhandarwada: "Bhandarwada",
+            Rahataghar: "Rahataghar", Ganapatipule: "Ganapatipule", Bhandarpule: "Bhandarpule", Neware: "Neware",
+            Kajirbhati: "Kajirbhati", Dhokambale: "Dhokambale", Kotavade: "Kotavade", Are: "Are",
+            Bholewadi: "Bholewadi", Basani: "Basani", Mayekarwadi: "Mayekarwadi", Kasarweli: "Kasarweli",
+            Sadamirya: "Sadamirya", Kalbadevi: "Kalbadevi", Shirgaon: "Shirgaon",
+             Jakimirya: "Jakimirya", Mirya: "Mirya", Zadgaon: "Zadgaon", Ratnagiri: "Ratnagiri",
+            'Kille ratnagiri': "Kille ratnagiri",Karle: "Karle", Bhatye: "Bhatye", Phansop: "Phansop",
+            Kasop: "Kasop", Wayangani: "Wayangani", Golap: "Golap", Ranpar: "Ranpar",
+            Musalmanwadi: "Musalmanwadi",Pavas: "Pavas", Ganespule: "Ganespule", 'Thikan Soman': "Thikan Soman",
+            Kurdhe: "Kurdhe", Mervi: "Mervi", Purnagad: "Purnagad", 'Peth Purnagad': "Peth Purnagad",
+            Bhandarwadi: "Bhandarwadi",Gaonkhadi: "Gaonkhadi"
+        },
+        Rajapur: {
+            Varachiwadi: "Varachiwadi", 'Avali Chiwadi': "Avali Chiwadi", Kasheli: "Kasheli", Wadavetye: "Wadavetye",
+            Wadativare: "Wadativare", Nate: "Nate", Ambolgad: "Ambolgad", Yashvantgad: "Yashvantgad",
+            Anatwadi: "Anatwadi", Bhandawadi: "Bhandawadi", Holi: "Holi",
+            Kuvesh: "Kuvesh", Mithgavane: "Mithgavane", Varilpada: "Varilpada", Madban: "Madban",
+            Bakale: "Bakale", Dandewadi: "Dandewadi", 'Shirse(Sagave)': "Shirse(Sagave)", 
+        }
+    },
+    Sindhudurg: {
+       
+        Devgad: {
+            Vijaydurg: "Vijaydurg", Rameshwar: "Rameshwar", Girye: "Girye", Hurshi: "Hurshi",
+            Kalambai: "Kalambai", Phanase: "Phanase", Padvane: "Padvane", Devgad: "Devgad",
+            Jamsande: "Jamsande", Mithmumbari: "Mithmumbari", Kunkeshwar: "Kunkeshwar",
+            Kathwaneshwar: "Kathwaneshwar", Katvan: "Katvan", 
+            Mithbaon: "Mithbaon", Tambaldeg: "Tambaldeg",
+            Hindale: "Hindale", Morve: "Morve", Munage: "Munage", Aadbandar: "Aadbandar"
+        },
+        Malwan: {
+            Gaudwadi: "Gaudwadi", jamdul: "jamdul", Pirawadi: "Pirawadi", Hirlewadi: "Hirlewadi",
+            Wayangani: "Wayangani", Tondavali: "Tondavali", 'Juva Pankhol': "Juva Pankhol", Hadi: "Hadi",
+            Shemadranewadi: "Shemadranewadi", Ozar: "Ozar", Revandi: "Revandi",
+            Kolamb: "Kolamb", Sarjekot: "Sarjekot", Nhive: "Nhive",'Malwani(MCI)': "Malwani(MCI)",
+            Kumbharmath: "Kumbharmath", Wayari: "Wayari", Kalethar: "Kalethar", Devli: "Devli",
+            Tarkali: "Tarkali"
+        },
+        Vengurla: {
+            Karli: "Karli", Bhogave: "Bhogave", Shelapi: "Shelapi", Shiriramwadi: "Shiriramwadi",
+            Kochre: "Kochre", Medha: "Medha", Mayana: "Mayana", Khavene: "Khavene",
+            Malai: "Malai", Kelus: "Kelus", Kalavi: "Kalavi",
+            Telekarwadi: "Telekarwadi", Wayangani: "Wayangani", Dabholi: "Dabholi", Vengurala: "Vengurala",
+            Navabag: "Navabag", Ubhadanda: "Ubhadanda", Parabwada: "Parabwada", Vagheshwar: "Vagheshwar",
+            Namas: "Namas", Siddhawadi: "Siddhawadi", Adari: "Adari",
+            Kamblewadi: "Kamblewadi", Sukhatanbag: "Sukhatanbag", Varachemad: "Varachemad", Kurlewadi: "Kurlewadi",
+            Girapwadi: "Girapwadi", Muth: "Muth", Mochemad: "Mochemad", Tank: "Tank",
+            Sakhelekhol: "Sakhelekhol", Temb: "Temb", Sangartirtha: "Sangartirtha",
+            Velgar: "Velgar", Bagayat: "Bagayat", Khalchikar: "Khalchikar", Varachikar: "Varachikar",
+            Sukhalbhat: "Sukhalbhat", Mhartale: "Mhartale", Redi: "Redi", 'Bombadoji Chiwadi': "Bombadoji Chiwadi",
+            Kanyale: "Kanyale", Gavatale: "Gavatale"
+        
+        }
+    }
+}
+
+window.onload = function () {
+    const selectDistrict = document.getElementById('district'),
+        selectTaluka = document.getElementById('taluka'),
+        selectVillage = document.getElementById('village'),
+        selects = document.querySelectorAll('select')
+
+    // selectDistrict.disabled = true
+    selectTaluka.disabled = true
+    selectVillage.disabled = true
+
+    selects.forEach(select => {
+        if (select.disabled == true) {
+            select.style.cursor = "auto"
+        }
+    })
+
+    for (let district in districtTalukaVillageinfo) {
+        console.log(district);
+        selectDistrict.options[selectDistrict.options.length] = new Option(district, district);
+    }
+    selectDistrict.onchange = (e) => {
+        selectDistrict.disabled = false
+        selectTaluka.disabled = true
+        selectVillage.disabled = true
+    }
+
+    //district change
+    selectDistrict.onchange = (e) => {
+        selectTaluka.disabled = false
+        selectVillage.disabled = true
+
+        selectTaluka.length = 1
+        selectVillage.length = 1
+
+        for (let taluka in districtTalukaVillageinfo[e.target.value]) {
+            console.log(taluka);
+            selectTaluka.options[selectTaluka.options.length] = new Option(taluka, taluka);
+        }
+    }
+
+    //taluka change
+    selectTaluka.onchange = (e) => {
+
+        selectVillage.disabled = false
+
+        selectVillage.length = 1
+
+        for (let village in districtTalukaVillageinfo[selectDistrict.value][e.target.value]) {
+            console.log(village);
+            selectVillage.options[selectVillage.options.length] = new Option(village, village);
+        }
+    }
+}
